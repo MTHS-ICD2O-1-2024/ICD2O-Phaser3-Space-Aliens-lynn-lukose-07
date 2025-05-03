7836b565f18462c183a6b3ee59c673a0bf7293e4
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-05-03 02:57:17 +0000

    edit titlescene

--- js/titleScene.js
+++ js/titleScene.js
@@ -14,7 +14,15 @@ class TitleScene extends Phaser.Scene {
    * This method is the constructor
    */
   constructor() {
-    super({ key: "titleScene"})
+    super({ key: "titleScene" })
+
+    this.titleSceneBackgroundImage = null
+    this.titleSceneText = null
+    this.titleSceneTextStyle = {
+      font: "200px Times",
+      fill: "#fde4b9",
+      align: "center",
+    }
   }
 
   /**
@@ -33,6 +41,7 @@ class TitleScene extends Phaser.Scene {
    */
   preload() {
     console.log("Title Scene")
+    this.preload.image("titleSceneBackground", "assets/aliens_screen_image.jpg")
   }
 
   /**
@@ -41,7 +50,18 @@ class TitleScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    // pass
+    this.titlehSceneBackgroundImage = this.add
+      .sprite(0, 0, "titleSceneBackground")
+      .setScale(2.75)
+    this.titleSceneBackgroundImage.x = 1920 / 2
+    this.titleSceneBackgroundImage.y = 1080 / 2
+
+    this.titleSceneText = this.add.text(
+      1920 / 2,
+      1080 / 2 + 350,
+      "Space Aliens!",
+      this.titleSceneTextStyle.setOrigin(0.5)
+    )
   }
 
   /**
@@ -55,4 +75,4 @@ class TitleScene extends Phaser.Scene {
   }
 }
 
-export default TitleScene 
+export default TitleScene

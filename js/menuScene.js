d4cc6d1127fed0ec111a97da85bd3b36819c42c2
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-05-03 02:38:38 +0000

    edited menu scene

---
+++ js/menuScene.js
@@ -0,0 +1,58 @@
+/* global Phaser */
+
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Lynn Lukose
+// Created on: Apr 2025
+// This is the Menu Scene
+
+/**
+ * This class is the Menu Scene.
+ */
+class MenuScene extends Phaser.Scene {
+  /**
+   * This method is the constructor
+   */
+  constructor() {
+    super({ key: "menuScene" })
+  }
+
+  /**
+   * Can be defined on your own scenes.
+   * This method is called by the Scene Manager when the scene starts,
+   *  before preload() and create().
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+
+  /**
+   * Can be defined on your own scenes.
+   * Use it to load assets.
+   */
+  preload() {
+    console.log("Menu Scene")
+  }
+
+  /**
+   * Can be defined on your own scenes.
+   * Use it to create your game objects.
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  create(data) {
+    // pass
+  }
+
+  /**
+   * should be overridden by my own scenes.
+   * method called once per game step while scene is running.
+   * @param {number} time - current time.
+   * @param {number} delta - The delta time in ms since last frame.
+   */
+  update(time, delta) {
+    // pass
+  }
+}
+
+export default MenuScene

fb7a7c2a8280dca9ae14453e01c1f1e77ee42a19
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-05-21 22:02:09 +0000

    formatty

--- js/gameScene.js
+++ js/gameScene.js
@@ -27,11 +27,16 @@ class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: "gameScene" })
 
+    this.background = null
     this.ship = null
     this.fireMissile = false
     this.score = 0
     this.scoreText = null
-    this.scoreTextStyle = {font: "65px Arial", fill: "ffffff", align: "center"}
+    this.scoreTextStyle = {
+      font: "65px Arial",
+      fill: "#ffffff",
+      align: "center",
+    }
   }
 
   /**
@@ -66,7 +71,12 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
-    this.scoreText = this.add.text(10, 10, "Score: " + this.score.toString(), this.scoreTextStyle)
+    this.scoreText = this.add.text(
+      10,
+      10,
+      "Score: " + this.score.toString(),
+      this.scoreTextStyle
+    )
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 

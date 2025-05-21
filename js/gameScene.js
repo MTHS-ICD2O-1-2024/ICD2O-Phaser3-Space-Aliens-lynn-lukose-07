5be04289dd1d860a464b846df850559ae193454b
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-05-21 21:37:08 +0000

    add collision

--- js/gameScene.js
+++ js/gameScene.js
@@ -54,27 +54,37 @@ class GameScene extends Phaser.Scene {
     this.load.image("ship", "./assets/spaceShip.png")
     this.load.image("missile", "./assets/missile.png")
     this.load.image("alien", "./assets/alien.png")
-    // sound
-    this.load.audio("laser", "./assets/laser1.wav")
+
+    //sound
+    this.load.audio("laser", "assets/laser1.wav")
+    this.load.audio("explosion", "assets/barrelExploding.wav")
   }
 
-  /**
-   * Can be defined on your own scenes.
-   * Use it to create your game objects.
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   create(data) {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
-    // create a group for the missiles
+    //create the group for the missiles
     this.missileGroup = this.physics.add.group()
 
-    // create a group for the aliens
+    //create the group for the aliens
     this.alienGroup = this.add.group()
     this.createAlien()
+
+    // Collisions between missiles and aliens
+    this.physics.add.collider(
+      this.missileGroup,
+      this.alienGroup,
+      function (missileCollide, alienCollide) {
+        alienCollide.destroy()
+        missileCollide.destroy()
+        this.sound.play("explosion")
+        this.createAlien()
+        this.createAlien()
+      }.bind(this)
+    )
   }
 
   /**
@@ -90,44 +100,44 @@ class GameScene extends Phaser.Scene {
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
     const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
-      if (keyLeftObj.isDown === true) {
-        this.ship.x -= 15
-        if (this.ship.x < 0) {
-          this.ship.x = 0
-        }
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x < 0) {
+        this.ship.x = 0
       }
+    }
 
-      if (keyRightObj.isDown === true) {
-        this.ship.x += 15
-        if (this.ship.x > 1920) {
-          this.ship.x = 1920
-        }
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
       }
+    }
 
-      if (keySpaceObj.isDown === true) {
-        if (this.fireMissile === false) {
-          // fire missile
-          this.fireMissile = true
-          const aNewMissile = this.physics.add.sprite(
-            this.ship.x,
-            this.ship.y,
-            "missile"
-          )
-          this.missileGroup.add(aNewMissile)
-          this.sound.play("laser")
-        }
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        // fire missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(
+          this.ship.x,
+          this.ship.y,
+          "missile"
+        )
+        this.missileGroup.add(aNewMissile)
+        this.sound.play("laser")
       }
+    }
 
-      if (keySpaceObj.isUp === true) {
-        this.fireMissile = false
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
+    }
+
+    this.missileGroup.children.each(function (item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
       }
-
-      this.missileGroup.children.each(function (item) {
-        item.y = item.y - 15
-        if (item.y < 0) {
-          item.destroy()
-        }
-      })
+    })
   }
 }
 

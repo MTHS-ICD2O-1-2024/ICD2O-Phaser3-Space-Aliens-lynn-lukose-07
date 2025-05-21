21acd9915c9a740f9f216db849cbd9a4f6c4c35e
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-05-21 21:25:26 +0000

    add comments

--- js/gameScene.js
+++ js/gameScene.js
@@ -13,9 +13,9 @@
 class GameScene extends Phaser.Scene {
   // create alien
   createAlien() {
-    const alienXLocation = Math.floor(Math.random() * 1920) + 1
-    let alienXVelocity = Math.floor(Math.random() * 50) + 1
-    alienXVelocity *= Math.round(Math.random()) ? 1 : -1
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1 //this will get a number between 1 and 50;
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1 //this will add minus sign in 50% of cases
     const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien")
     anAlien.body.velocity.y = 200
     anAlien.body.velocity.x = alienXVelocity

485ef2d0f8db11939bc931d2587d4c2b96fe0010
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-05-22 13:22:23 -0400

    Update gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -120,7 +120,7 @@ class GameScene extends Phaser.Scene {
         alienCollide.destroy()
         this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again", this.gameOverTextStyle).setOrigin(0.5)
         this.gameOverText.setInteractive({useHandCursor: true})
-        this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
+        this.gameOverText.on("pointerdown", () => this.scene.start("menuScene"))
       }.bind(this)
     )
   }
